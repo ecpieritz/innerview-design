e963b6aeb4117375631632f496306c8382a32054
ecpieritz <ecpieritz@gmail.com>
2023-11-01 10:16:56 -0300

    Added Clients base

---
+++ src/pages/Clients.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Clients() {
+  return (
+    <main className='id-clients'>
+      <div className="container-fluid">
+        <h1>Clients</h1>
+      </div>
+    </main>
+  )
+}
+
+export default Clients
