ea1d4a434e554c8d88a76499c998e1fb61851876
ecpieritz <ecpieritz@gmail.com>
2023-11-01 13:32:40 -0300

    Added scroll function to navbar

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import { Outlet } from "react-router-dom"
 import Navbar from '../components/Navbar'
 
 function Layout() {
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const handleScroll = () => {
+    const navbar = document.querySelector('.id-navbar');
+    if (window.scrollY > 50) {
+      navbar.style.background = '#9F0006'; // Defina a cor de fundo desejada
+    } else {
+      navbar.style.background = 'transparent';
+    }
+  };
   return (
     <>
       <Navbar />
