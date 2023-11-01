ecd35ef937539fddad6f83bbc2fc13b405c545b5
ecpieritz <ecpieritz@gmail.com>
2023-11-01 10:14:29 -0300

    Added Home base

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <main className='id-home'>
+      <div className="container-fluid">
+        <h1>Home</h1>
+      </div>
+    </main>
+  )
+}
+
+export default Home
