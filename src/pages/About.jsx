049496fa50b70bdfeb0fb22b0b0e043b5c0adf93
ecpieritz <ecpieritz@gmail.com>
2023-11-01 10:15:33 -0300

    Added About base

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+  return (
+    <main className='id-about'>
+      <div className="container-fluid">
+        <h1>Home</h1>
+      </div>
+    </main>
+  )
+}
+
+export default About
