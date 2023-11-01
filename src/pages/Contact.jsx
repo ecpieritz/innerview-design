f5612fe70eb17297f46a0bc276d0c62c56fc18fb
ecpieritz <ecpieritz@gmail.com>
2023-11-01 10:17:30 -0300

    Added Contact base

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <main className='id-contact'>
+      <div className="container-fluid">
+        <h1>Contact</h1>
+      </div>
+    </main>
+  )
+}
+
+export default Contact
