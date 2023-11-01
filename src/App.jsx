36018dc6e2ebb5983d141c50524d311ad3d54e3a
ecpieritz <ecpieritz@gmail.com>
2023-11-01 10:20:41 -0300

    Added route paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./scss/index.scss";
 
-function App() {
+import About from "./pages/About";
+import Clients from "./pages/Clients";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Layout from "./pages/Layout";
+import Projects from "./pages/Projects";
+
+export default function App() {
   return (
-    <>
-      <h1>It's working!</h1>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="clients" element={<Clients />} />
+          <Route path="contact" element={<Contact />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
