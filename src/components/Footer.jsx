d40a9b9ae4f4c29588ad46363c3ac3f2b350bd5d
ecpieritz <ecpieritz@gmail.com>
2023-11-03 12:38:37 -0300

    Added white footer on About and Contact

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useLocation } from 'react-router-dom';
+
 
 function Footer() {
+  const location = useLocation();
+  const isAboutOrContact = location.pathname === '/about' || location.pathname === '/contact';
+
   return (
-    <footer className='id-footer'>
+    <footer className={`id-footer ${isAboutOrContact ? 'white' : ''}`}>
       <div className="container-fluid">
         <a className='id-footer__logo' href="/"><p>InnerView</p></a>
 
