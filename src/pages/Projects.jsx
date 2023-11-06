1d2f70d942a51c10aab0a72707cfc9b03d49201a
ecpieritz <ecpieritz@gmail.com>
2023-11-06 17:34:28 -0300

    Added Projects list items

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -2,10 +2,30 @@ import React from "react";
 import ProjectsItem from "../components/Projects/ProjectsItem";
 
 function Projects() {
+  const projects = [
+    {
+      title: "aaa",
+      description: "bbb"
+    },
+    {
+      title: "ddd",
+      description: "eee"
+    },
+    {
+      title: "fff",
+      description: "ggg"
+    },
+    {
+      title: "hhh",
+      description: "iii"
+    },
+  ]
 
   return (
     <main className="id-projects">
-        <ProjectsItem  />
+      {projects.map(project => (
+        <ProjectsItem  title={project.title} description={project.description} />
+      ))}
     </main>
   );
 }
