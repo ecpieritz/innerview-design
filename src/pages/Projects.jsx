3aa39427dc074fbf47f01ba39f1299b06b767af7
ecpieritz <ecpieritz@gmail.com>
2023-11-06 17:33:44 -0300

    Added Projects base

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React from "react";
+import ProjectsItem from "../components/Projects/ProjectsItem";
 
 function Projects() {
+
   return (
-    <main className='id-projects'>
-      <div className="container-fluid">
-        <h1>Projects</h1>
-      </div>
+    <main className="id-projects">
+        <ProjectsItem  />
     </main>
-  )
+  );
 }
 
-export default Projects
+export default Projects;
