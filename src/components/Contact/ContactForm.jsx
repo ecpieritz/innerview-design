369a801df81caf7622ccb7c0f50ac9c460d0f278
ecpieritz <ecpieritz@gmail.com>
2023-11-06 17:50:14 -0300

    changed class do className

--- src/components/Contact/ContactForm.jsx
+++ src/components/Contact/ContactForm.jsx
@@ -6,24 +6,24 @@ function ContactForm() {
         <h2>Contact</h2>
         <hr />
       <form>
-        <div class="form-group">
+        <div className="form-group">
           <div className="container">
-            <input type="text" class="form-control" id="inputName" placeholder="Name" />
+            <input type="text" className="form-control" id="inputName" placeholder="Name" />
           </div>
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <div className="container">
-            <input type="email" class="form-control" id="inputEmai" placeholder="Email" />
+            <input type="email" className="form-control" id="inputEmai" placeholder="Email" />
           </div>
         </div>
-        <div class="form-group">
+        <div className="form-group">
           <div className="container">
             <textarea id="inputMessage" placeholder="Message" />
           </div>
         </div>
         <div className="id-contact__form__btn">
           <div className="container">
-            <button type="submit" class="id-btn form">
+            <button type="submit" className="id-btn form">
               <p>Submit</p>
             </button>
           </div>
