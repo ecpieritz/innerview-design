94eb9986e8a728846fd74e5f89337720d2bacb7e
ecpieritz <ecpieritz@gmail.com>
2023-11-07 09:52:31 -0300

    Added SingleProject banner

---
+++ src/components/Single/SingleBanner.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import bannerImg from '../../assets/img/project01/id-projects__single__banner.jpg'
+
+export default function SingleBanner() {
+  return (
+    <div className='id-project-single__banner'>
+      <img src={bannerImg} alt="banner" />
+    </div>
+  )
+}
