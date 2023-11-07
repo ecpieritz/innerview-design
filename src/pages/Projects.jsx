fae3bda70348aaa652dbd2e6411c188a0bc91b59
ecpieritz <ecpieritz@gmail.com>
2023-11-07 08:55:52 -0300

    Added projects data

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -4,20 +4,20 @@ import ProjectsItem from "../components/Projects/ProjectsItem";
 function Projects() {
   const projects = [
     {
-      title: "aaa",
-      description: "bbb"
+      title: "Showcasing the Beautiful Spectrum of Skin Tones",
+      description: "Explore the richness of differences in our Profile Pictures Project. Each portrait reveals the unique beauty of individuals' varying skin tones. A celebration of authenticity and the diversity that makes us all unique."
     },
     {
-      title: "ddd",
-      description: "eee"
+      title: "Beauty of Working Women: A Photographic Tribute",
+      description: "In this project, we capture the essence of strength and beauty in women who play pivotal roles in various fields of work. Each image is a tribute to the resilience and grace of working women."
     },
     {
-      title: "fff",
-      description: "ggg"
+      title: "Time-Traveling Through Photography",
+      description: "Travel back in time with our Vintage-Inspired Fashion Photography Project, drawing inspiration from the 1920s to the 1960s. Each image pays homage to the timeless elegance and iconic fashion of those eras, infusing a hint of nostalgia into the present day."
     },
     {
-      title: "hhh",
-      description: "iii"
+      title: "Underwear: Celebrating Everyday Fashion",
+      description: "This project celebrates the beauty of everyday fashion, highlighting the comfort, style, and authenticity of everyday underwear, an integral part of our daily lives. It's a heartfelt tribute to fashion that's intimately connected to both our hearts and bodies."
     },
   ]
 
