04619a4881e0f6a74fc54dee0996bb75e8a41093
ecpieritz <ecpieritz@gmail.com>
2023-11-07 09:08:07 -0300

    Added ProjectsItem structure

--- src/components/Projects/ProjectsItem.jsx
+++ src/components/Projects/ProjectsItem.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
 
-function ProjectsItem({title, description }) {
+function ProjectsItem({title, description, id }) {
   return (
     <section className="id-projects__item">
       <div className="container-fluid">
-        <h2>{title}</h2>
-        <p>{description}</p>
+        <div className="id-projects__item__text">
+          <h2>{title}</h2>
+          <div className="row">
+            <div className="col-md-2">
+              <h3>{id}</h3>
+            </div>
+            <div className="col-md-8">
+              <p>{description}</p>
+            </div>
+            <div className="col-md-2">
+              <a className="id-btn arrow white" href="/contact">
+                View Project <FontAwesomeIcon icon={faArrowRightLong} />
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
