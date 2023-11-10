940c3f3daee75bac69eb373f0671ec109c5a07a4
ecpieritz <ecpieritz@gmail.com>
2023-11-10 12:56:05 -0300

    Added ContactMap

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ContactForm from '../components/Contact/ContactForm'
+import ContactMap from '../components/Contact/ContactMap'
 
 function Contact() {
   return (
     <main className='id-contact'>
+        <ContactMap />
         <ContactForm />
     </main>
   )
